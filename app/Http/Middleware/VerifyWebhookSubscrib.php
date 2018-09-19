f096c2ab183871af778dd09d6c46140520d525a8
Andy <andy_huange@webgene.com.tw>
2018-09-19 18:57:48 +0800

    Add and Register Verify middleware and Add route

---
+++ app/Http/Middleware/VerifyWebhookSubscrib.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class VerifyWebhookSubscrib
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $data = $request->all();
+        if ($data["hub.mode"] === "subscribe"
+            && $data["hub.verify_token"] === env("MESSENGER_VERIFY_TOKEN")) {
+            return response($data["hub.challenge"], 200);
+        }
+        return $next($request);
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/webhook', 'MessengerController@recive')->middleware('verify');
